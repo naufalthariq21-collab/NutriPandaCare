4513f973096cb95bec0f17dde0a4a9a44f0a7ab4
naufal <naufalthariq21@gmail.com>
2026-04-10 22:40:34 +0700

    commit firebase done

--- app/src/main/java/com/example/nutripandacare/MainActivity.kt
+++ app/src/main/java/com/example/nutripandacare/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.nutripandacare
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/nutripandacare/RegistrationActivity.kt
+++ app/src/main/java/com/example/nutripandacare/RegistrationActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 import androidx.core.view.WindowInsetsCompat
 
 class RegistrationActivity : AppCompatActivity() {
