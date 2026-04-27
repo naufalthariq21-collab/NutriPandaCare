9971eee0c39ff3ecc5a263dbfca827cc92cc6245
naufal <naufalthariq21@gmail.com>
2026-04-27 10:30:31 +0700

    commit kt activity login

--- app/src/main/java/com/example/nutripandacare/DashboardGuruActivity.kt
+++ app/src/main/java/com/example/nutripandacare/DashboardGuruActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nutripandacare.databinding.ActivityDashboardGuruBinding
 
 class DashboardGuruActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardGuruBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard_guru)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        
+        binding = ActivityDashboardGuruBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/nutripandacare/DashboardPengelolaActivity.kt
+++ app/src/main/java/com/example/nutripandacare/DashboardPengelolaActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.nutripandacare.databinding.ActivityDashboardPengelolaBinding
 
 class DashboardPengelolaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardPengelolaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard_pengelola)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        
+        binding = ActivityDashboardPengelolaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
